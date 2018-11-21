7fa1c4994636e6023c3abc150209c59cccb84ffc
djgar98 <djgar98@gmail.com>
2018-11-21 10:19:35 -0600

    Cambio de Activity

--- PruebaPills/app/src/main/java/com/pruebaexample/prueba/MainActivity.java
+++ PruebaPills/app/src/main/java/com/pruebaexample/prueba/MainActivity.java
@@ -1,14 +1,28 @@
 package com.pruebaexample.prueba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlayout = findViewById(R.id.btnlayout1);
+         btnlayout.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent i = new Intent(MainActivity.this,Activity2.class);
+                 startActivity(i);
+             }
+         });
+
     }
 }
